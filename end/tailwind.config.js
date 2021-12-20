6a20ecc8d53e1e889ebe4e3a5301b9b9cbe06308
unknown <nguyenthuyvy20152016@gmail.com>
2021-12-20 21:09:16 +0700

    update...

---
+++ end/tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: [
+    "**/*.html"
+  ],
+  darkMode: 'class', // or 'media' or 'class'
+  theme: {
+    
+    fontFamily: {
+      body: "'Inter', sans-serif"
+    },
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
